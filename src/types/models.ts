d4ccff728f687753abceff4f7cbb9bdb34f3aaf8
Primemerlinian <mwatson0518@gmail.com>
2023-02-24 17:57:18 -0500

    correct repo

---
+++ src/types/models.ts
@@ -0,0 +1,32 @@
+/* ---------===== custom props ====--------- */
+
+
+
+/* ---------===== auth models =====--------- */
+
+export interface Profile {
+  name: string;
+  photo?: string;
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface User {
+  name: string;
+  email: string;
+  profile: { id: number };
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+
+export interface Meme {
+  id: number;
+  profileId: number;
+  photo?: string;
+  caption: string;
+  createdAt: string;
+  updatedAt: string;
+}
