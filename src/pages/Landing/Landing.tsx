646e840cf7438b6e2eb753dbde50623439a2d6b1
Primemerlinian <mwatson0518@gmail.com>
2023-03-01 01:50:12 -0500

    add typescript logo to readme

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -15,6 +15,8 @@ const Landing = (props: LandingProps): JSX.Element => {
     <main className={styles.container}>
       <h1 style={{ color: 'red' }}>Welcome to MemeScore!</h1>
       <h2 style={{ color: 'white'}} >MemeScore lets you add and view funny memes! Log in and check it out!</h2>
+      <img src="https://i.imgur.com/KZE7z34.png" alt="description of image">
+
     </main>
   )
 }
