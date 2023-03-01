0426afc68a5cd83bfeb5ecb334115010a1ba3d22
Primemerlinian <mwatson0518@gmail.com>
2023-03-01 02:09:15 -0500

    fix landing page photo

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -13,10 +13,9 @@ const Landing = (props: LandingProps): JSX.Element => {
 
   return (
     <main className={styles.container}>
-      <h1 style={{ color: 'red' }}>Welcome to MemeScore!</h1>
-      <h2 style={{ color: 'white'}} >MemeScore lets you add and view funny memes! Log in and check it out!</h2>
-      {/* <img src="https://i.imgur.com/KZE7z34.png" alt="description of image"> */}
-
+    <h1 style={{ color: 'red' }}>Welcome to MemeScore!</h1>
+    <h2 style={{ color: 'white' }}>MemeScore lets you add and view funny memes! Log in and check it out!</h2>
+    <img src="https://i.imgur.com/KZE7z34.png" alt="description of image"/>
     </main>
   )
 }
