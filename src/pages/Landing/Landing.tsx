e434d12ee784605feb444d81e8cb9e3c3ddcf7bf
Primemerlinian <mwatson0518@gmail.com>
2023-03-01 02:25:12 -0500

    add finishing touches to readme

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,23 +1,25 @@
 // stylesheets
-import styles from './Landing.module.css'
+import styles from "./Landing.module.css";
 
 // types
-import { User } from '../../types/models'
+import { User } from "../../types/models";
 
 interface LandingProps {
   user: User | null;
 }
 
 const Landing = (props: LandingProps): JSX.Element => {
-  const { user } = props
+  const { user } = props;
 
   return (
     <main className={styles.container}>
-    <h1 style={{ color: 'red' }}>Welcome to MemeScore!</h1>
-    <h2 style={{ color: 'white' }}>MemeScore lets you add and view funny memes! Log in and check it out!</h2>
-    <img src="https://i.imgur.com/KZE7z34.png"/>
+      <h1 style={{ color: "red" }}>Welcome to MemeScore!</h1>
+      <h2 style={{ color: "white" }}>
+        MemeScore lets you add and view funny memes! Log in or Sign Up and check it out!
+      </h2>
+      <img src="https://i.imgur.com/KZE7z34.png" />
     </main>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
